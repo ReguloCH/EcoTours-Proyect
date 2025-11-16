5fb5761e22b3dd1dad67378d02d53e0821fd3cf3
R0meus <jesus011030@outlook.com>
2025-11-15 20:02:11 -0400

    Modifique el navbar para que funcione con bootstrap (no pude hacer que se quedara en su posición mientras haces scroll) Cree y redirigí como principal la pagina para que seleccionen si usuario o administrador (Hay que ponerle un logo con mejor resolución) Acomode unos espacios en la pagina principal En contáctanos acomode que el form chocaba con el footer Quite el navbar de la pagina que la pagina que seleccionan su rol Quite del navbar la opción de iniciar sesión como administrador Quite los archivos de bt icons ya que no se llamaban de forma local Se elimino el archivo de prueba HelloWorld.vue Se arreglo el fondo de copyright para que se vieran las letras

---
+++ EcoTours-vue/src/Inicio.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="pag-principal d-flex flex-column min-vh-100">
+    <!-- Contenedor principal -->
+    <div class="container flex-grow-1 d-flex align-items-center justify-content-center">
+      <div class="row w-100 justify-content-center">
+        <div class="col-md-6 col-lg-5">
+          <!-- Contenedor usando lo de grid -->
+          <div class="card border-0 shadow-lg p-4 rounded-4 login-box" style="background-color: rgba(255,255,255,0.92);margin-top: 40px;">
+            <img src="/img/EcoToursLOGO.png" alt="Logo de EcoTours" class="img-fluid">
+            <br>  
+            <h1 class="text-center mb-4">Eligue tu rol</h1>
+              <div class="d-grid">
+                <router-link to="/PagPrincipal" class="btn btn-warning btn-lg fw-semibold text-white">Cliente</router-link>
+              </div>
+              <br>
+              <div class="d-grid">
+                <router-link to="/adminLOGIN" class="btn btn-warning btn-lg fw-semibold text-white">Administrador</router-link>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import FooterIni from './components/footerIni.vue';
+
+export default {
+  name: 'iniciarSesion',
+  components: {
+    FooterIni
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    iniciarSesion() {
+      if (this.email && this.password) {
+        // Redirige a la página de clientes
+        this.$router.push('/pagina-clientes');
+      } else {
+        alert('Por favor, completa todos los campos.');
+      }
+    },
+    irARegistro() {
+      // Redirige al formulario de registro
+      this.$router.push('/registrate');
+    },
+  },
+};
+</script>
